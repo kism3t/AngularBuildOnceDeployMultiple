d6bb7bcb3edc502640220a9e45810a97bff343c1
de313e <ufuk.altin@kiongroup.com>
2022-03-10 13:13:00 +0100

    - using App Config Service make it work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppConfigService } from "./services/app-config.service";
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
 
-const initConfig = (configService: AppConfigService) => {
-  return ()=>  configService.init();
-}
+const appInitializerFn = (appConfig: AppConfigService) => {
+  return () => {
+    return appConfig.loadAppConfig();
+  };
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule,
   ],
   providers: [
+    AppConfigService,
     {
       provide: APP_INITIALIZER,
-      useFactory: initConfig,
+      useFactory: appInitializerFn,
       multi: true,
       deps: [AppConfigService]
     }
